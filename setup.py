0ae1f01391088e36105906a841f88cf269905884
harih4ran <hariharank.offl@gmail.com>
2023-04-21 23:43:31 +0530

    folder structure aranged

--- setup.py
+++ setup.py
@@ -7,17 +7,17 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Django Viewers Count Middleware get from user request'
 
 # Setting up
 setup(
-    name="django-viewers-count-middleware",
+    name="django-viewers-counts-middleware",
     version=VERSION,
     author="Hariharan",
     author_email="hari@ittamil.in",
     description=DESCRIPTION,
-    url = "https://github.com/ittamil/django-viewers-count-middleware",
+    url = "https://github.com/ittamil/django-viewers-counts-middleware",
     packages=find_packages(),
     keywords=['python', 'django', 'viewers count middleware', 'viewers count', 'middleware', 'django plugin'],
     classifiers=[
