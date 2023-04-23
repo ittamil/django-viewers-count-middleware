af7940179abf24cb5c968cf06778dee05acf0a7f
Simone Pozzoli <simonepozzoli1@gmail.com>
2023-04-23 23:59:57 +0200

    App verbose name

--- viewers_count/apps.py
+++ viewers_count/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 class ViewersCountMiddlewareConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'viewers_count'
+    verbose_name = 'Viewers Count'
 
